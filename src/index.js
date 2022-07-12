0b150928bbc79fabe918ff465f48c63464df0f72
Mahesh <deshmukhmahesh009@gmail.com>
2022-07-12 20:16:19 +0530

    refactor: provider added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { DepartmentsProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <DepartmentsProvider>
+      <App />
+    </DepartmentsProvider>
   </React.StrictMode>
 );
